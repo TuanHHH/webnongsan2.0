52d302d6c2b99bcfdb6587f023cf37c22d2163bc
hungp03 <ph.hung0903@gmail.com>
2024-10-18 19:08:17 +0700

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,6 +35,9 @@ const App = () => {
           <Route path="/error" element={<ErrorPage />} />
           <Route path='*' element={<ErrorPage />}></Route>
         </Route>
+        <Route path={path.ADMIN_LAYOUT} element={<Admin/>}>
+          
+        </Route>
         <Route path={path.MEMBER} element={<MemberLayout />}>
           <Route path={path.PERSONAL} element={<Personal />}></Route>
           <Route path={path.HISTORY} element={<History />}></Route>
