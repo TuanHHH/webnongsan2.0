e4ba2f2e8eb0bc602389b4edb1593a489e8ff4ad
hungp03 <ph.hung0903@gmail.com>
2024-10-18 19:00:07 +0700

    fix bug import

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@
 import React, { useEffect } from 'react'
 import path from '@/utils/path'
 import { Route, Routes} from "react-router-dom";
-import { Login, Home, Public, ProductDetail, ForYou, Product, ResetPassword, CartDetail, Checkout, ErrorPage } from "@/pages/guest";
+import { Login, Home, Public, ProductDetail, ForYou, Product, ResetPassword, CartDetail, Checkout, ErrorPage, LocationSelector} from "@/pages/guest";
 import { MemberLayout, Personal, Wishlist, History } from '@/pages/member';
 import { useDispatch, useSelector } from "react-redux";
 import { getCategories } from "@/store/app/asyncActions";
@@ -33,9 +33,11 @@ const App = () => {
           <Route path={path.CART} element={<CartDetail />}></Route>
           <Route path={path.CHECKOUT} element={<Checkout />}></Route>
           <Route path={path.RESET_PASSWORD} element={<ResetPassword />}></Route>
-          <Route path='/admin' element={<Admin/>}></Route>
           <Route path="/error" element={<ErrorPage />} />
           <Route path='*' element={<ErrorPage />}></Route>
+        </Route>
+        <Route path={path.ADMIN} element={<Admin/>}>
+          
         </Route>
         <Route path={path.MEMBER} element={<MemberLayout />}>
           <Route path={path.PERSONAL} element={<Personal />}></Route>
